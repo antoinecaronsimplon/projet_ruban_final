0879f230dc8d2d705ddaa4c7281a193163ada04a
AntoineCARON <antoinecaron.simplon@gmail.com>
2017-12-15 09:42:58 +0100

    projet_final

---
+++ js/app.js
@@ -0,0 +1,63 @@
+
+$(document).ready(function(){
+    var clicked = false;
+  $(".ribbon-ciseaux").hover(function(){
+      if($(this).hasClass("zoomed")){
+      $(this).removeClass("zoomed");
+      }else{
+      $(this).addClass("zoomed");
+      }
+  });
+  $(".ribbon-ciseaux").click(function(){
+      if($(this).hasClass("zoomed")){
+        $(this).removeClass("zoomed");
+      }else{
+          $(this).addClass("zoomed");
+      }
+      var e = jQuery.Event("keydown");
+      e.which = 39; // # Key code
+          $("#test").trigger(e);
+  });
+});
+
+ 
+      $(function() {      
+        var Page = (function() {
+          var $navArrows = $( '#nav-arrows' ),
+            $nav = $( '#nav-dots > span' ),
+            slitslider = $( '#slider' ).slitslider( {
+              onBeforeChange : function( slide, pos ) {
+                $nav.removeClass( 'nav-dot-current' );
+                $nav.eq( pos ).addClass( 'nav-dot-current' );
+              }
+            } ),
+            init = function() {
+              initEvents();             
+            },
+            initEvents = function() {
+              // add navigation events
+              $navArrows.children( ':last' ).on( 'click', function() {
+
+                slitslider.next();
+                return false;
+              } );
+              $navArrows.children( ':first' ).on( 'click', function() {               
+                slitslider.previous();
+                return false;
+              } );
+              $nav.each( function( i ) {  
+                $( this ).on( 'click', function( event ) {
+                  var $dot = $( this );
+                  if( !slitslider.isActive() ) {
+                    $nav.removeClass( 'nav-dot-current' );
+                    $dot.addClass( 'nav-dot-current' );                 
+                  }               
+                  slitslider.jump( i + 1 );
+                  return false;               
+                } );            
+              } );
+            };
+            return { init : init };
+        })();
+        Page.init();    
+      });
